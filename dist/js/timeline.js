3ae2290de8625252795aebcb695fe85ac2d0bd6c
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2014-08-03 20:44:02 -0300

    Added events to timeline

--- dist/js/timeline.js
+++ dist/js/timeline.js
@@ -29,34 +29,174 @@ angular.module('animates.angular-timeline', [])
 			template:	"",
 			scope: {
 				point: '=',
+				eventData: '=',
+				pointMove: "&",
+				pointClick: "&",
+				multiplepointeventSelected: "&"
 			},
 			link: function ($scope, element, attrs) {
 				element.css({
 					left: $scope.point.tick + 'px'
 				});
 
-				element.addClass('timeline-point');
+				element.on('click', function () {
+						$scope.eventClick({
+							eventData : $scope.eventData,
+							pointData : $scope.point.data
+						});
+
+						$scope.multiplepointeventSelected({
+							eventData : $scope.eventData
+						});
+					});
 			}
 		};
 	})
-	.directive('animatesTimelineevent', function () {
+	.directive('animatesTimelineevent', function ($document) {
 		return {
 			restrict: 'E',
 			template:	"",
 			scope: {
 				evt: '=',
+				eventStartchange: "&",
+				eventDurationchange: "&",
+				eventClick: "&"
 			},
 			link: function ($scope, element, attrs) {
-				var evt = $scope.evt;
+				var evt = $scope.evt,
+					x, originalDuration;
+
 				element.css({
 					left: evt.start + 'px',
-					width: evt.duration + 'px',
+					width: evt.duration + 'px'
 				});
 
-				element.addClass('timeline-event');
 				element.addClass(evt.class);
 
-				//element.on('click', function (){alert('hola' );});
+				element.on('click', function () {
+					$scope.eventClick({
+						eventData : $scope.evt.data
+					});
+				});
+
+				element.on('mouseover', function(event) {
+					var y = event.pageX - evt.start;
+
+					if (y <= 10) {
+						element.css({
+							'cursor': 'ew-resize'
+						});
+					} else if ((evt.duration - y) <= 10) {
+						element.css({
+							'cursor': 'ew-resize'
+						});
+					} else {
+						element.css({
+							'cursor': 'move'
+						});
+					}
+				});
+
+				element.on('mousedown', function(event) {
+					// Prevent default dragging of selected content
+					event.preventDefault();
+					x = event.pageX - evt.start;
+					originalDuration = evt.duration;
+
+					element.css({
+						'z-index': 1000
+					});
+
+					if (x <= 10) {
+						$document.on('mousemove', elementExpandFront);
+						$document.on('mouseup', elementExpandFrontEnd);
+					} else if ((evt.duration - x) <= 10) {
+						$document.on('mousemove', elementExpandBack);
+						$document.on('mouseup', elementExpandBackEnd);
+					} else {
+						$document.on('mousemove', elementMove);
+						$document.on('mouseup', elementMoveEnd);
+					}
+				});
+
+				function elementMove(event) {
+					var newStart = event.pageX - x;
+					if (newStart > 0) {
+						evt.start = newStart;
+						element.css({
+							left: newStart + 'px'
+						});
+					}
+				}
+
+				function elementMoveEnd() {
+					$document.unbind('mousemove', elementMove);
+					$document.unbind('mouseup', elementMoveEnd);
+
+					$scope.eventStartchange({
+						eventData : $scope.evt.data,
+						newStartTick : evt.start
+					});
+				}
+
+				function elementExpandFront(event) {
+					var newStart = event.pageX - x,
+							newDuration = (newStart - evt.start) + originalDuration;
+
+					if (newStart > 0 && newDuration > 0) {
+						evt.duration = newDuration;
+						evt.start = newStart;
+						element.css({
+							left: newStart + 'px',
+							width: evt.duration + 'px'
+						});
+					}
+				}
+
+				function elementExpandFrontEnd() {
+					$document.unbind('mousemove', elementExpandFront);
+					$document.unbind('mouseup', elementExpandFrontEnd);
+
+					$scope.eventStartchange({
+						eventData : $scope.evt.data,
+						newStartTick : evt.start
+					});
+
+					$scope.eventDurationchange({
+						eventData : $scope.evt.data,
+						newDuration : evt.duration
+					});
+				}
+
+				function elementExpandBack(event) {
+					var newStart = event.pageX - x,
+						newDuration = (newStart - evt.start) + originalDuration;
+						console.log ('------');
+console.log ('x: ' + x);
+						console.log ('event.x: ' + event.x);
+								console.log ('event.pageX: ' + event.pageX);
+				console.log ('evt.duration: ' + evt.duration);
+				console.log ('newStart: ' + newStart);
+				console.log ('(newStart - evt.start): ' + (newStart - evt.start));
+				console.log ('newDuration: ' + newDuration);
+
+					if (newDuration > 0) {
+						evt.duration = newDuration;
+						element.css({
+							width: evt.duration + 'px'
+						});
+					}
+				}
+
+				function elementExpandBackEnd() {
+					$document.unbind('mousemove', elementExpandBack);
+					$document.unbind('mouseup', elementExpandBackEnd);
+
+					$scope.eventDurationchange({
+						eventData : $scope.evt.data,
+						newDuration : evt.duration
+					});
+				}
 			}
 		};
 	})
@@ -64,35 +204,186 @@ angular.module('animates.angular-timeline', [])
 		return {
 			restrict: 'E',
 			template:	"<div class='timeline-content'>" +
-									"<animates-timelinepoint ng-repeat='point in data.points' point='point'> </animates-timelinePoint>" +
-									"<animates-timelineevent ng-repeat='event in data.events' evt='event'> </animates-timelineevent>" +
+									"<animates-timelinepoint class='timeline-point' ng-repeat='point in line.points' point='point' event-data='line.data' " +
+										"point-move='internalPointMove(pointData, newTick)' " +
+										"point-click='internalPointClick(pointData)' " +
+										"multiplepointevent-selected='internalMultiplePointEventSelected(eventData)' " +
+										"> </animates-timelinepoint>" +
+
+									"<animates-timelineevent class='timeline-event' ng-repeat='event in line.events' evt='event' " +
+										"event-startchange='internalEventStartChange(eventData, newStartTick)' " +
+										"event-durationchange='internalEventDurationChange(eventData, newDuration)' " +
+										"event-click='internalEventClick(eventData)' " +
+										"> </animates-timelineevent>" +
 								"</div>",
 			scope: {
-				data: '=',
+				line: '=data',
+				timelineData: '=',
+				eventStartchange: "&",
+				eventDurationchange: "&",
+				eventClick: "&",
+				pointMove: "&",
+				pointClick: "&",
+				multiplepointeventSelected: "&"
+			},
+			controller : function ($scope) {
+				$scope.internalEventStartChange = function (eventData, newStartTick) {
+					if ($scope.eventStartchange) {
+						$scope.eventStartchange({
+							timelineData : $scope.timelineData,
+							eventData : eventData,
+							newStartTick : newStartTick
+						});
+					}
+				};
+
+				$scope.internalEventDurationChange = function (eventData, newDuration) {
+					if ($scope.eventDurationchange) {
+						$scope.eventDurationchange({
+							timelineData : $scope.timelineData,
+							eventData : eventData,
+							newDuration : newDuration
+						});
+					}
+				};
+
+				$scope.internalEventClick = function (eventData) {
+					if ($scope.eventClick) {
+						$scope.eventClick({
+							timelineData : $scope.timelineData,
+							eventData : eventData
+						});
+					}
+				};
+
+				$scope.internalPointMove = function (pointData, newTick) {
+					if ($scope.pointMove) {
+						$scope.pointMove({
+							timelineData : $scope.timelineData,
+							pointData : pointData,
+							newTick : newTick
+						});
+					}
+				};
+
+				$scope.internalPointClick = function (pointData) {
+					if ($scope.pointClick) {
+						$scope.pointClick({
+							timelineData : $scope.timelineData,
+							pointData : pointData
+						});
+					}
+				};
+
+				$scope.internalMultiplePointEventSelected = function (eventData) {
+					if ($scope.multiplepointeventSelected) {
+						$scope.multiplepointeventSelected({
+							timelineData : $scope.timelineData,
+							eventData : eventData
+						});
+					}
+				};
 			}
 		};
 	})
 	.directive('animatesTimelines', function () {
 		return {
 			restrict: 'E',
-			template: "<input type='number' ng-change='tickchange();' ng-model='tick'></input>" +
-						"<span>{{tick}}</input>" +
+			template: "<input type='number' ng-change='internalTickChange()' ng-model='tick'></input>" +
+						"<span>{{tick}}</span>" +
 						"<div class='timelines-group' >" +
 							"<div class='timelinesHeaders'>" +
-								"<div ng-repeat='timeline in data' class='timeline-part timeline-header' id='{{timeline.guid}}'>" +
+								"<div ng-repeat='timeline in data' class='timeline-part timeline-header' data='timeline.data'>" +
 									"<span class='timeline-header-track'>{{timeline.name}}</span>" +
 								"</div>" +
 							"</div>" +
 							"<div class='timelinesContainer'>" +
-								"<div ng-repeat='timeline in data' class='timeline-part timeline' id='{{timeline.guid}}'>" +
-									"<animates-timeline ng-repeat='line in timeline.lines' data='line' >" +
+								"<div ng-repeat='timeline in data' class='timeline-part timeline' data='timeline.data'>" +
+									"<animates-timeline ng-repeat='line in timeline.lines' data='line' timeline-data='timeline.data' " +
+										"point-move='internalPointMove(timelineData, pointData, newTick)' " +
+										"point-click='internalPointClick(timelineData, pointData)' " +
+										"multiplepointevent-selected='internalMultiplePointEventSelected(timelineData, eventData)' " +
+										"event-startchange='internalEventStartChange(timelineData, eventData, newStartTick)' " +
+										"event-durationchange='internalEventDurationChange(timelineData, eventData, newDuration)' " +
+										"event-click='internalEventClick(timelineData, eventData)' " +
+									"> </animates-timeline>" +
 								"</div>" +
 							"</div>" +
 						"</div>",
 			scope: {
 				data: '=',
 				tick: '=',
-				tickchange: "&"
+				tickChange: "&",
+				eventStartchange: "&",
+				eventDurationchange: "&",
+				eventClick: "&",
+				pointMove: "&",
+				pointClick: "&",
+				multiplepointeventSelected: "&"
+			},
+			controller : function ($scope) {
+				$scope.internalTickChange = function () {
+					if ($scope.tickChange) {
+						$scope.tickChange( { tick: $scope.tick });
+					}
+				};
+
+				$scope.internalEventStartChange = function (timelineData, eventData, newStartTick) {
+					if ($scope.eventStartchange) {
+						$scope.eventStartchange({
+							timelineData : timelineData,
+							eventData : eventData,
+							newStartTick : newStartTick
+						});
+					}
+				};
+
+				$scope.internalEventDurationChange = function (timelineData, eventData, newDuration) {
+					if ($scope.eventDurationchange) {
+						$scope.eventDurationchange({
+							timelineData : timelineData,
+							eventData : eventData,
+							newDuration : newDuration
+						});
+					}
+				};
+
+				$scope.internalEventClick = function (timelineData, eventData) {
+					if ($scope.eventClick) {
+						$scope.eventClick({
+							timelineData : timelineData,
+							eventData : eventData
+						});
+					}
+				};
+
+				$scope.internalPointMove = function (timelineData, pointData, newTick) {
+					if ($scope.pointMove) {
+						$scope.pointMove({
+							timelineData : timelineData,
+							pointData : pointData,
+							newTick : newTick
+						});
+					}
+				};
+
+				$scope.internalPointClick = function (timelineData, pointData) {
+					if ($scope.pointClick) {
+						$scope.pointClick({
+							timelineData : timelineData,
+							pointData : pointData
+						});
+					}
+				};
+
+				$scope.internalMultiplePointEventSelected = function (timelineData, eventData) {
+					if ($scope.multiplepointeventSelected) {
+						$scope.multiplepointeventSelected({
+							timelineData : timelineData,
+							eventData : eventData
+						});
+					}
+				};
 			}
 		};
 	});
--- examples/examples.js
+++ examples/examples.js
@@ -1,25 +1,27 @@
 'use strict';
 
 var module = angular.module('demo', ['animates.angular-timeline'])
-  .controller('DemoTimelineGroupCtrl',  function ($scope) {
+  .controller('DemoTimelineGroupCtrl', function ($scope) {
     $scope.tick = 200;
     $scope.timelines =
         [
           {
-            guid : 'timeline1',
             name : 'timeline1-name',
+            data : { id : 'timeline1' },
             lines : [
               {
                 events :
                 [
                   {
                     name : 'animation1',
+                    data : { id : 'animation1-guid' },
                     start : 10,
                     duration : 190,
                     'class' : 'fade-out',
                   },
                   {
                     name : 'animation2',
+                    data : { id : 'animation2-guid' },
                     start : 210,
                     duration : 200,
                     'class' : 'fade-in',
@@ -27,15 +29,16 @@ var module = angular.module('demo', ['animates.angular-timeline'])
                 ]
               }, {
                 name : 'multiAnimation1',
+                data : { id : 'multiAnimation1-guid' },
                 points :
                 [
                   {
                     tick : 10,
-                    id : 'guid1',
+                    data : { id : 'guid1' },
                   },
                   {
                     tick : 210,
-                    id : 'guid2',
+                    data : { id : 'guid2' },
                   }
                 ]
               }, {
@@ -44,33 +47,35 @@ var module = angular.module('demo', ['animates.angular-timeline'])
                 [
                   {
                     tick : 24,
-                    id : 'guid7',
+                    data : { id : 'guid7' },
                   },
                   {
                     tick : 104,
-                    id : 'guid5',
+                    data : { id : 'guid5' },
                   },
                   {
                     tick : 250,
-                    id : 'guid6',
+                    data : { id : 'guid6' },
                   }
                 ]
               }
             ]
           }, {
-            guid : 'timeline2',
             name : 'timeline2-name',
+            data : { id : 'timeline2-guid' },
             lines : [
                 {
                   events :
                   [
                     {
                       name : 'animation3',
+                      data : { id : 'animation3-guid' },
                       start : 100,
                       duration : 100
                     },
                     {
                       name : 'animation4',
+                      data : { id : 'animation4-guid' },
                       start : 500,
                       duration : 450
                     }
@@ -91,19 +96,21 @@ var module = angular.module('demo', ['animates.angular-timeline'])
                 }
               ]
             }, {
-              guid : 'timeline4',
               name : 'timeline4-name',
+              data : { id : 'timeline4-guid' },
               lines : [
                 {
                   events :
                   [
                     {
                       name : 'animation6',
+                      data : { id : 'animation6-guid' },
                       start : 150,
                       duration : 100
                     },
                     {
                       name : 'animation7',
+                      data : { id : 'animation7-guid' },
                       start : 500,
                       duration : 4500
                     }
@@ -113,8 +120,53 @@ var module = angular.module('demo', ['animates.angular-timeline'])
             }
         ];
 
-      $scope.onTickChange = function (newVal, oldVal) {
-        alert(newVal);
+      $scope.onTickChange = function (tick) {
+        alert(tick);
+      };
+
+      $scope.onPointMove = function (timelineData, pointData, newTick) {
+        console.log('onPointMove');
+        console.log(timelineData);
+        console.log(pointData);
+        console.log(newTick);
+        console.log('------');
+      };
+
+      $scope.onPointClick = function (timelineData, pointData) {
+        console.log('onPointClick');
+        console.log(timelineData);
+        console.log(pointData);
+        console.log('------');
+      };
+
+      $scope.onMultiplePointEventSelected = function (timelineData, eventData) {
+        console.log('onMultiplePointEventSelected');
+        console.log(timelineData);
+        console.log(eventData);
+        console.log('------');
+      };
+
+      $scope.onEventStartChange = function (timelineData, eventData, newStartTick) {
+        console.log('onEventStartChange');
+        console.log(timelineData);
+        console.log(eventData);
+        console.log(newStartTick);
+        console.log('------');
+      };
+
+      $scope.onEventDurationChange = function (timelineData, eventData, newDuration) {
+        console.log('onEventDurationChange');
+        console.log(timelineData);
+        console.log(eventData);
+        console.log(newDuration);
+        console.log('------');
+      };
+
+      $scope.onEventClick = function (timelineData, eventData){
+        console.log('onEventClick');
+        console.log(timelineData);
+        console.log(eventData);
+        console.log('------');
       };
 
       $scope.changeTick = function (){
